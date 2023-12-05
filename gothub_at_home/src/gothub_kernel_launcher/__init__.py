1629a3f4313b20b3f91b3a45da00238664c449a2
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-12-04 18:14:45 -0800

    ok

--- gothub_at_home/src/gothub_kernel_launcher/__init__.py
+++ gothub_at_home/src/gothub_kernel_launcher/__init__.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def main():
-    print("Hello from gothub_kernel_launcher!")
+    """
+    Copied from
+    https://github.com/ipython/ipykernel/blob/e9ddcf50a30b59d63f585ffa9ffffd368095a54a/ipykernel_launcher.py#L9
+    """
+
+    # Remove the CWD from sys.path while we load stuff.
+    # This is added back by InteractiveShellApp.init_path()
+    if sys.path[0] == "":
+        del sys.path[0]
+
+    from ipykernel import kernelapp as app
+
+    app.launch_new_instance()
