2bc7f57c17c45d785a4dc6fa5cbc590c46fbf557
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-12-05 01:57:34 -0800

    ok

--- gothub_at_home/src/gothub_kernel_launcher/kernels/chatgpt_kernel.py
+++ gothub_at_home/src/gothub_kernel_launcher/kernels/chatgpt_kernel.py
@@ -97,7 +97,7 @@ class ChatGptKernel(IPythonKernel):
                 }
                 self.send_response(
                     self.iopub_socket,
-                    "warning",
+                    "error",
                     stream_content,
                 )
 
