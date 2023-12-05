7a2174b26cb239d72feddc28acd0fae7b7cced13
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-12-04 18:20:52 -0800

    nit

--- gothub_at_home/src/gothub_kernel_launcher/__main__.py
+++ gothub_at_home/src/gothub_kernel_launcher/__main__.py
@@ -1,5 +1,17 @@
-from . import main
+"""
+Copied from
+https://github.com/ipython/ipykernel/blob/e9ddcf50a30b59d63f585ffa9ffffd368095a54a/ipykernel_launcher.py#L9
+"""
 
+
+import sys
 
 if __name__ == "__main__":
-    main()
+    # Remove the CWD from sys.path while we load stuff.
+    # This is added back by InteractiveShellApp.init_path()
+    if sys.path[0] == "":
+        del sys.path[0]
+
+    from ipykernel import kernelapp as app
+
+    app.launch_new_instance()
