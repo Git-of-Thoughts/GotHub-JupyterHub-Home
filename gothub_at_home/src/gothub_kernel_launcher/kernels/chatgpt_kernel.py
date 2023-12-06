354d237aec714293f9c249688c7beeaf17f7db72
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-12-06 02:33:40 -0800

    it does make a difference

--- gothub_at_home/src/gothub_kernel_launcher/kernels/chatgpt_kernel.py
+++ gothub_at_home/src/gothub_kernel_launcher/kernels/chatgpt_kernel.py
@@ -165,7 +165,7 @@ summary::-webkit-details-marker {{
         You said:
         <span class="icon">👇</span>
     </summary>
-    <md-span># what {code}</md-span>
+    <md-span># what {html.escape(code)}</md-span>
 </details>
 """,
                     },
