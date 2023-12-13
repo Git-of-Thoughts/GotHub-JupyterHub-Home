6f523649d15bbf1934704a0e35570bed4ad32cbb
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-12-13 14:44:17 -0800

    does it work?

--- src/gothub_kernel_launcher/kernels/super_king/__init__.py
+++ src/gothub_kernel_launcher/kernels/super_king/__init__.py
@@ -1,14 +1,29 @@
+from typing import TYPE_CHECKING
+
 import requests
 
 from gothub_kernel_launcher.kernels.configs import SERVER_TIMEOUT, server_sub_url
 from gothub_kernel_launcher.kernels.utils import firebase
 
+if TYPE_CHECKING:
+    from gothub_kernel_launcher.kernels.chatgpt_kernel import ChatGptKernel
 
-def super_king_debug(self):
-    pass
+
+def super_king_debug(self: "ChatGptKernel"):
+    stream_content = {
+        "metadata": {},
+        "data": {
+            "text/html": '<iframe src="https://wikipedia.com/"></iframe>',
+        },
+    }
+    self.send_response(
+        self.iopub_socket,
+        "display_data",
+        stream_content,
+    )
 
 
-def chat_through_firebase_realtime_db(self):
+def chat_through_firebase_realtime_db(self: "ChatGptKernel"):
     user_folder = f"/chat/{self.user_id}"
 
     child_key = firebase.db.child(
