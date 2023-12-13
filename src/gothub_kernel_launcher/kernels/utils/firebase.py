55a634df2f7673b85767e8b2114ad83fa3a1599e
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-12-12 18:25:09 -0800

    forgot this

--- src/gothub_kernel_launcher/kernels/utils/firebase.py
+++ src/gothub_kernel_launcher/kernels/utils/firebase.py
@@ -2,14 +2,16 @@ import firebase
 
 # Firebase configuration
 config = {
-    "apiKey": "apiKey",
-    "authDomain": "projectId.firebaseapp.com",
-    "databaseURL": "https://databaseName.firebaseio.com",
-    "projectId": "projectId",
-    "storageBucket": "projectId.appspot.com",
-    "messagingSenderId": "messagingSenderId",
-    "appId": "appId",
+    "apiKey": "AIzaSyBX_nGfqOKq9t4G1200krLBsmm3XscJGsI",
+    "authDomain": "gothub-dataengine.firebaseapp.com",
+    "databaseURL": "https://gothub-dataengine-default-rtdb.firebaseio.com",
+    "projectId": "gothub-dataengine",
+    "storageBucket": "gothub-dataengine.appspot.com",
+    "messagingSenderId": "752753357845",
+    "appId": "1:752753357845:web:dbb61366b8df0c67440a1c",
+    "measurementId": "G-TYR0BJFRTG",
 }
+
 
 # Instantiates a Firebase app
 app = firebase.initialize_app(config)
