d491fa8b087d1ef03a1fd4c4dfcb7bf82000b890
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-12-12 22:43:01 -0800

    something like this

--- src/gothub_kernel_launcher/kernels/super_king/__init__.py
+++ src/gothub_kernel_launcher/kernels/super_king/__init__.py
@@ -1,2 +1,14 @@
-def super_king_debug(self):
-    pass
+import requests
+
+from gothub_kernel_launcher.kernels.chatgpt_kernel import ChatGptKernel
+from gothub_kernel_launcher.kernels.configs import server_sub_url
+
+
+def super_king_debug(self: ChatGptKernel):
+    requests.post(
+        server_sub_url("chat"),
+        json={
+            "user_id": self.user_id,
+            "message": "super king debug",
+        },
+    )
