ed15aed0a933961b0fd1ea87dca2f3d6ba5b04a4
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-12-12 18:33:54 -0800

    ..

--- src/gothub_kernel_launcher/kernels/chatgpt_kernel.py
+++ src/gothub_kernel_launcher/kernels/chatgpt_kernel.py
@@ -28,9 +28,9 @@ _my_firebase_password_response = requests.get(
         "GotHub-API-Key": GOTHUB_API_KEY,
     },
 )
-_my_firebase_password_response.raise_for_status()
-_my_firebase_password_json = _my_firebase_password_response.json()
-FIREBASE_USER = _my_firebase_password_json
+# _my_firebase_password_response.raise_for_status()
+# _my_firebase_password_json = _my_firebase_password_response.json()
+# FIREBASE_USER = _my_firebase_password_json
 
 
 # Model
@@ -129,7 +129,7 @@ class ChatGptKernel(IPythonKernel):
             print_firebase_regex = r"^\s*print\s+firebase\s*$"
             if re.match(print_firebase_regex, code):
                 return self.do_execute(
-                    f"as code: {FIREBASE_USER}",
+                    f"as code: {None}",
                     silent,
                     store_history,
                     user_expressions,
