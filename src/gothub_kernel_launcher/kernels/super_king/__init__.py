59dda84ae7dc08af1a14758e793dd1ed30949453
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-12-13 00:34:07 -0800

    do this

--- src/gothub_kernel_launcher/kernels/super_king/__init__.py
+++ src/gothub_kernel_launcher/kernels/super_king/__init__.py
@@ -5,19 +5,26 @@ from gothub_kernel_launcher.kernels.utils import firebase
 
 
 def super_king_debug(self):
-    response = requests.post(
-        server_sub_url("chat"),
-        json={
-            "user_id": self.user_id,
-            "messages": "super king debug",
-        },
-    )
-    response.raise_for_status()
-    response_json = response.json()
+    # response = requests.post(
+    #     server_sub_url("chat"),
+    #     json={
+    #         "user_id": self.user_id,
+    #         "messages": "super king debug",
+    #     },
+    # )
+    # response.raise_for_status()
+    # response_json = response.json()
 
-    ref_path = response_json["ref_path"]
+    # ref_path = response_json["ref_path"]
 
     def callback(event):
         self._ChatGptKernel__print(str(event))
 
-    firebase.db.child(ref_path).stream(callback, is_async=False)
+    firebase.db.child(
+        f"/chat/{self.user_id}",
+    ).push(
+        {
+            "user_id": self.user_id,
+            "messages": "super king debug",
+        }
+    ).stream(callback, is_async=False)
